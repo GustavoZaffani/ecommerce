e55e7109aff2e17ba1e6203a89d4ae311544f6eb
g_zaffani@hotmail.com <gu18071999>
2019-10-19 11:22:15 -0300

    Initial commit

---
+++ src/main/java/br/edu/utfpr/projeto/parte2/commerce/CommerceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.projeto.parte2.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/utfpr/projeto/parte2/commerce/CommerceApplicationTests.java
@@ -0,0 +1,14 @@
+package br.edu.utfpr.projeto.parte2.commerce;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
